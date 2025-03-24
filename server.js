8bf788f752df5d19d9a2c398326bd68f4d4977ae
Andrei Tamse <113184924+Brasprin@users.noreply.github.com>
2025-03-24 10:24:41 +0800

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const port = 8080;
-
-app.use(express.static(__dirname));
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
