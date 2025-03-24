97803d41d974b5d26d842b63453c1ceba0a9ee01
Andrei Tamse <113184924+Brasprin@users.noreply.github.com>
2025-03-24 10:24:04 +0800

    Delete js directory

--- js/sidebar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("components/sidebar.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("sidebar-container").innerHTML = data;
-        })
-        .catch(error => console.error("Error loading sidebar:", error));
-});
