d874b183aeae5f8573d26ec84167283286a4d50c
Andrei Tamse <113184924+Brasprin@users.noreply.github.com>
2025-05-03 17:26:57 +0800

    Add files via upload

--- js/sidebar.js
+++ js/sidebar.js
@@ -5,4 +5,23 @@ document.addEventListener("DOMContentLoaded", function () {
             document.getElementById("sidebar-container").innerHTML = data;
         })
         .catch(error => console.error("Error loading sidebar:", error));
+
+    fetch("components/mobileSidebar.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("mobileSidebar-container").innerHTML = data;
+
+        const toggleButton = document.getElementById('sidebarToggle');
+        const mobileSidebar = document.getElementById('mobile-sidebar');
+
+        if (toggleButton && mobileSidebar) {
+            toggleButton.addEventListener('click', function () {
+                mobileSidebar.classList.toggle('show');
+            });
+        } else {
+            console.error("Toggle button or sidebar not found.");
+        }
+    })
+    .catch(error => console.error("Error loading mobile sidebar:", error));
+
 });
